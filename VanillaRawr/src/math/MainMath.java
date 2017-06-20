036282446857921f3b6c483cd34e67cadfe2dfbe
Haeldeus <nordein.vail@yahoo.de>
2017-06-20 18:31:15 +0200

    Added a Math package
    Math package added. Will be used in later stages to do the Math to find
    the Best Equipment

---
+++ VanillaRawr/src/math/MainMath.java
@@ -0,0 +1,18 @@
+package math;
+
+import java.util.ArrayList;
+
+import util.Item;
+//import util.MultiBonus; //Later used to create Item variation
+
+public class MainMath {
+
+	private static int getMana(/*TODO*/) {
+		
+		return 0;
+	}
+	
+	public static void doMath(ArrayList<Item> items, int[] talents, util.Class c) {
+		getMana();
+	}
+}
